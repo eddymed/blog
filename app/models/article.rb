6505c6dbbefe8793ee253d34ead4420f159294f0
eddymed <medinalavado@gmail.com>
2022-03-31 18:49:37 -0400

    Crea Article model y validacion

---
+++ app/models/article.rb
@@ -0,0 +1,5 @@
+class Article < ApplicationRecord
+    validates :title, presence: true, length: { minumum: 3, maximum: 60}
+    
+    validates :description, presence: true, , length: { minumum: 10, maximum: 500}
+end
