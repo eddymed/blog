8c1b59fce8380947b00c791fa2492900a58b02ef
eddymed <medinalavado@gmail.com>
2022-03-31 21:01:32 -0400

    Display table, destroy method

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,5 +1,5 @@
 class Article < ApplicationRecord
     validates :title, presence: true, length: { minumum: 3, maximum: 60}
     
-    validates :description, presence: true, , length: { minumum: 10, maximum: 500}
+    validates :description, presence: true, length: { minumum: 10, maximum: 500}
 end
